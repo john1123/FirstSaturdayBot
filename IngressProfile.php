3c669d96c84d6dfa1f8281eac98a95f1c9124643
Ivan Kelembetov <kelembetov@gmail.com>
2020-02-27 16:02:50 +0300

    Изменение параметра Didact

--- IngressProfile.php
+++ IngressProfile.php
@@ -56,7 +56,7 @@ class IngressProfile
         'Intel Ops Missions',
         'Stealth Ops Missions',
         'Umbra: Unique Resonator Slots Deployed',
-        'Didact: Total Fields Created',
+        'Didact Fields Created',
         'Recursions',
     ];
     /** @var $firstValuesPhrase - Very first parameters name */
